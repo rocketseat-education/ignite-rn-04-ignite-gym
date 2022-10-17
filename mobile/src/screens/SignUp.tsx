3ddafdbe4f7c41c5d0a296f00b569b27a1e39bc2
RennanD <rplaykl@gmail.com>
2022-10-17 16:00:59 -0300

    Fetch API - Async Await

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -36,17 +36,18 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password }: FormDataProps) {
-    fetch('http://localhost:3333/users', {
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch('http://localhost:3333/users', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ name, email, password })
-    })
-      .then(response => response.json())
-      .then(data => console.log(data));
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
