756e9bf4de14f1dd5bbae241275bf6e259a8bd3f
RennanD <rplaykl@gmail.com>
2022-10-19 16:20:19 -0300

    HTTP Client - Entendendo Interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
+
+api.interceptors.response.use((response) => {
+  console.log("INTERCEPTOR =>", response)
+  return response
+}, (error) => {
+  console.log('INTERCEPTOR RESPONSE ERROR =>', error)
+  return Promise.reject(error);
+})
+
+export { api };
