2e7c4565bf6cd2d2efc6fbcf1bcf1977255c6367
RennanD <rplaykl@gmail.com>
2022-10-19 16:36:05 -0300

    HTTP Client - Tratando Erros da API

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,15 +1,16 @@
 import { Alert } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import axios from 'axios';
 import { api } from "@services/api";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
+import { AppError } from '@utils/AppError';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -30,6 +31,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+
+  const toast = useToast();
   
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -46,9 +49,15 @@ export function SignUp() {
       const response = await api.post('/users', { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if(axios.isAxiosError(error)) {
-        Alert.alert(error.response?.data.message);
-      }
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     }
   }
 
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 
+import { AppError } from "@utils/AppError";
+
 const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
 
-api.interceptors.response.use((response) => {
-  console.log("INTERCEPTOR =>", response)
-  return response
-}, (error) => {
-  console.log('INTERCEPTOR RESPONSE ERROR =>', error)
-  return Promise.reject(error);
+api.interceptors.response.use((response) => response, (error) => {
+  if(error.response && error.response.data) {
+    return Promise.reject(new AppError(error.response.data.message))
+  } else {
+    return Promise.reject(error)
+  }
+  
 })
 
 export { api };
