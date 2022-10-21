f3dd458de2309fcb9d5a5decbb7160667b37675a
RennanD <rplaykl@gmail.com>
2022-10-21 16:50:01 -0300

    Contextos no React Native - Unificando o create e provider context

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { Routes } from '@routes/index';
 
-import { AuthContext } from '@contexts/AuthContext';
+import { AuthContextProvider } from '@contexts/AuthContext';
 
 import { THEME } from './src/theme';
 
@@ -20,16 +20,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider value={{
-        user: {
-          id: '1',
-          name: 'Rodrigo Gonçalves',
-          email: 'rodrigo@email.com',
-          avatar: 'rodrigo.png'
-        }
-      }}>
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -6,4 +6,23 @@ export type AuthContextDataProps = {
   user: UserDTO;
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode
+}
+
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps)  {
+  return (
+    <AuthContext.Provider value={{
+      user: {
+        id: '1',
+        name: 'Rodrigo Gonçalves',
+        email: 'rodrigo@email.com',
+        avatar: 'rodrigo.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
