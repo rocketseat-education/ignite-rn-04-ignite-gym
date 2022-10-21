1690b645b2519984f7d0b45b9d3e9c9da422fdf3
RennanD <rplaykl@gmail.com>
2022-10-21 17:03:27 -0300

    Contextos no React Native - Compartilhando estados no context

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -13,15 +13,16 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps)  {
+
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Rodrigo',
+    email: 'rodrigo@email.com',
+    avatar: 'rodrigo.png'
+  })
+
   return (
-    <AuthContext.Provider value={{
-      user: {
-        id: '1',
-        name: 'Rodrigo Gonçalves',
-        email: 'rodrigo@email.com',
-        avatar: 'rodrigo.png'
-      }
-    }}>
+    <AuthContext.Provider value={{ user }}>
       {children}
     </AuthContext.Provider>
   )
