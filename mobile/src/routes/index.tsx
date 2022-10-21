9bf51a93a31ae3e0d489ade43b06904ffaa91cf3
RennanD <rplaykl@gmail.com>
2022-10-21 16:59:44 -0300

    Contextos no React Native - Criando seu próprio Hook

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
@@ -11,9 +11,9 @@ import { AppRoutes } from "./app.routes";
 export function Routes() {
   const { colors } = useTheme();
 
-  const contextData = useContext(AuthContext);
+  const { user } = useAuth();
 
-  console.log("USUÁRIO LOGADO =>", contextData);
+  console.log("USUÁRIO LOGADO =>", user);
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
