72b7128324f5f68a1d6a7c288a68da0f968f1a17
RennanD <rplaykl@gmail.com>
2022-10-24 17:48:51 -0300

    Contextos no React Native - Redirecionar usuário logado

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -13,15 +13,13 @@ export function Routes() {
 
   const { user } = useAuth();
 
-  console.log("USUÁRIO LOGADO =>", user);
-
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
