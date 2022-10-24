9740952ae25bd7bbfe2cac02b0750a97bb790f76
RennanD <rplaykl@gmail.com>
2022-10-24 17:00:20 -0300

    Contextos no React Native - Tratando Exceções do handleSignIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,14 @@
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 import { useAuth } from '@hooks/useAuth';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
+import { AppError } from '@utils/AppError';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -19,6 +21,8 @@ type FormData = {
 export function SignIn() {
   const { singIn } = useAuth();
 
+  const toas = useToast();
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
@@ -27,8 +31,20 @@ export function SignIn() {
     navigation.navigate('signUp');
   }
 
-  async function handleSignIn({ email, password }: FormData){
-    await singIn(email, password);
+  async function handleSignIn({ email, password }: FormData) {
+    try {
+      await singIn(email, password);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+ 
+      const title =  isAppError ? error.message : 'Não foi possível entrar. Tente novamente mais tarde.'
+    
+      toas.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
   }
 
   return (
