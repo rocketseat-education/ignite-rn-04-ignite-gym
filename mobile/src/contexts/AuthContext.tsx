d69c3c06ffc2a42292962d0ed6a244437e05824e
RennanD <rplaykl@gmail.com>
2022-10-25 15:58:19 -0300

    Autenticação JWT - Obtendo Token do usuário

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -27,9 +27,9 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
     try {
       const { data } = await api.post('/sessions', { email, password });
      
-      if(data.user) {
+      if(data.user && data.token) {
         setUser(data.user);
-        storageUserSave(data.user)
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error
