3beb4fcd718b045484d871ca16ad83526cb72f07
RennanD <rplaykl@gmail.com>
2022-10-25 17:27:51 -0300

    Consumindo o Back-end - Exibindo exercícios por grupo

--- mobile/src/components/ExerciseCard.tsx
+++ mobile/src/components/ExerciseCard.tsx
@@ -3,11 +3,13 @@ import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
 
 import { Entypo } from '@expo/vector-icons';
 
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
+
 type Props = TouchableOpacityProps & {
-  
+  data: ExerciseDTO;
 };
 
-export function ExerciseCard({ ...rest }: Props) {
+export function ExerciseCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity {...rest}>
       <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
@@ -23,11 +25,11 @@ export function ExerciseCard({ ...rest }: Props) {
 
         <VStack flex={1}>
           <Heading fontSize="lg" color="white" fontFamily="heading">
-            Remanda unilateral
+            {data.name}
           </Heading>
 
           <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
-            3 séries x 12 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
 
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -107,7 +107,10 @@ export function Home() {
           data={exercises}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <ExerciseCard onPress={handleOpenExerciseDetails} />
+            <ExerciseCard 
+              onPress={handleOpenExerciseDetails}
+              data={item}
+            />
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
