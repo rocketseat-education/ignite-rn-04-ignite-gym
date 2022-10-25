896bca6a58134494c8f9e813fef1468e155d63d5
RennanD <rplaykl@gmail.com>
2022-10-25 17:37:01 -0300

    Consumindo o Back-end - Carregando imagem

--- mobile/src/components/ExerciseCard.tsx
+++ mobile/src/components/ExerciseCard.tsx
@@ -3,6 +3,8 @@ import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
 
 import { Entypo } from '@expo/vector-icons';
 
+import { api } from '@services/api';
+
 import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 type Props = TouchableOpacityProps & {
@@ -14,7 +16,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
     <TouchableOpacity {...rest}>
       <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
         <Image 
-          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
           alt="Imagem do exercício"
           w={16}
           h={16}
