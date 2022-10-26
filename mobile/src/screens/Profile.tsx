0efdbdff90dbda47e17ba4c6135d132a7dcef3ec
RennanD <rplaykl@gmail.com>
2022-10-26 18:22:37 -0300

    Consumindo o Back-end - Atualizando usuário no back-end

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -9,6 +9,9 @@ import * as yup from 'yup';
 
 import { useAuth } from '@hooks/useAuth';
 
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
+
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
@@ -50,7 +53,7 @@ const profileSchema = yup.object({
 })
 
 export function Profile() {
-
+  const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
 
@@ -103,7 +106,27 @@ export function Profile() {
   }
 
   async function handleProfileUpdate(data: FormDataProps) {
-    console.log(data);
+    try {
+      setIsUpdating(true);
+      await api.put('/users', data);
+
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível atualizar os dados. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsUpdating(false);
+    }
   }
  
   return (
@@ -214,6 +237,7 @@ export function Profile() {
             title="Atualizar" 
             mt={4} 
             onPress={handleSubmit(handleProfileUpdate)}
+            isLoading={isUpdating}
           />
         </Center>
       </ScrollView>
