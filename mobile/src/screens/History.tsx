407675d2166391367f7549cdadfb615a191b78b5
RennanD <rplaykl@gmail.com>
2022-10-26 15:26:14 -0300

    Consumindo o Back-end - Carregando histórico

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
-import { Heading, VStack, SectionList, Text } from 'native-base';
+import { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
+  const [isLoading, setIsLoading] = useState(true);
   const [exercises, setExercises] = useState([
     {
       title: '26.08.22',
@@ -15,6 +20,35 @@ export function History() {
       data: ["Puxada frontal"]
     }
   ]);
+
+  const toast = useToast();
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+      const response = await api.get('/history');
+
+      console.log(response.data);
+
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível carregar os detalhes do exercício';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory()
+    },[])
+  )
 
   return (
     <VStack flex={1}>
