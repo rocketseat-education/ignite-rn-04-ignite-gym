c7208d8d0224b7586a051f9db6af97807929bebf
RennanD <rplaykl@gmail.com>
2022-10-26 19:01:22 -0300

    Consumindo o Back-end - Atualizando dados do usuário no storage e estado

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -9,6 +9,7 @@ import { UserDTO } from "@dtos/UserDTO";
 export type AuthContextDataProps = {
   user: UserDTO;
   singIn: (email: string, password: string) => Promise<void>;
+  updateUserProfile: (userUpdated: UserDTO) => Promise<void>;
   signOut: () => Promise<void>;
   isLoadingUserStorageData: boolean;
 }
@@ -71,6 +72,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
     }
   }
 
+  async function updateUserProfile(userUpdated: UserDTO) {
+    try {
+      setUser(userUpdated);
+      await storageUserSave(userUpdated);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async function loadUserData() {
     try {
       setIsLoadingUserStorageData(true);
@@ -96,6 +106,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
     <AuthContext.Provider value={{ 
       user, 
       singIn,
+      updateUserProfile,
       signOut,
       isLoadingUserStorageData
     }}>
--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -58,7 +58,7 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
 
   const toast = useToast();
-  const { user } = useAuth();
+  const { user, updateUserProfile } = useAuth();
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({ 
     defaultValues: { 
       name: user.name,
@@ -108,7 +108,13 @@ export function Profile() {
   async function handleProfileUpdate(data: FormDataProps) {
     try {
       setIsUpdating(true);
+
+      const userUpdated = user;
+      userUpdated.name = data.name;
+
       await api.put('/users', data);
+
+      await updateUserProfile(userUpdated);
 
       toast.show({
         title: 'Perfil atualizado com sucesso!',
