95c8e2d6ae721ec3c7ee5b91362b5dd9a667ed0f
RennanD <rplaykl@gmail.com>
2022-10-26 16:43:32 -0300

    Consumindo o Back-end - Validando a senha

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -27,7 +27,8 @@ type FormDataProps = {
 
 const profileSchema = yup.object({
   name: yup.string().required('Informe o nome'),
-
+  password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos.').nullable().transform((value) => !!value ? value : null),
+  confirm_password: yup.string().nullable().transform((value) => !!value ? value : null).oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.'),
 })
 
 export function Profile() {
