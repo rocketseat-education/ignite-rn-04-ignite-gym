cccc46a56ce5f9dbda5d09c95d13056baeb90241
RennanD <rplaykl@gmail.com>
2022-10-26 16:50:51 -0300

    Consumindo o Back-end - Validação condicional da senha

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -28,7 +28,15 @@ type FormDataProps = {
 const profileSchema = yup.object({
   name: yup.string().required('Informe o nome'),
   password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos.').nullable().transform((value) => !!value ? value : null),
-  confirm_password: yup.string().nullable().transform((value) => !!value ? value : null).oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.'),
+  confirm_password: yup
+    .string()
+    .nullable()
+    .transform((value) => !!value ? value : null)
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
+    .when('password', {
+      is: (Field: any) => Field, 
+      then: yup.string().nullable().required('Informe a confirmação da senha.')
+    }),
 })
 
 export function Profile() {
