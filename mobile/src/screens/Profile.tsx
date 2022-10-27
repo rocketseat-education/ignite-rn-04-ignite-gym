677809532c02bd85095d610b0147deaf6ffed9e7
RennanD <rplaykl@gmail.com>
2022-10-27 17:25:26 -0300

    Upload de Imagem - Preparando dados da foto

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -95,7 +95,15 @@ export function Profile() {
           })
         }
 
-        setUserPhoto(photoSelected.uri);
+        const fileExtension = photoSelected.uri.split('.').pop();
+
+        const photoFile = {
+          name: `${user.name}.${fileExtension}`.toLowerCase(),
+          uri: photoSelected.uri,
+          type: `${photoSelected.type}/${fileExtension}`
+        }
+
+        console.log(photoFile);
       }
   
     } catch (error) {
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AppError } from "@utils/AppError";
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.19:3333'
+  baseURL: 'http://localhost:3333'
 });
 
 api.interceptors.response.use((response) => response, (error) => {
