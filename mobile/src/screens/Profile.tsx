6389daeb71168224a123e03e9c3c3e267d603aac
RennanD <rplaykl@gmail.com>
2022-10-27 17:35:46 -0300

    Upload de Imagem - Fazendo o Upload da imagem

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -101,9 +101,23 @@ export function Profile() {
           name: `${user.name}.${fileExtension}`.toLowerCase(),
           uri: photoSelected.uri,
           type: `${photoSelected.type}/${fileExtension}`
-        }
+        } as any;
 
-        console.log(photoFile);
+        const userPhotoUploadForm = new FormData();
+
+        userPhotoUploadForm.append('avatar', photoFile);
+
+        await api.patch('/users/avatar', userPhotoUploadForm, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        toast.show({
+          title: 'Foto atualizada!',
+          placement: 'top',
+          bgColor: 'green.500'
+        })
       }
   
     } catch (error) {
