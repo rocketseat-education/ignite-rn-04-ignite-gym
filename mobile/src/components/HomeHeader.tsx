b6b5ebcf6ce71e7b0cc0c28431aedc2689e75786
RennanD <rplaykl@gmail.com>
2022-10-27 17:46:46 -0300

    Upload de Imagem - Exibindo o avatar atualizado

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -4,6 +4,8 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 import { useAuth } from '@hooks/useAuth';
 
+import { api } from '@services/api';
+
 import defaulUserPhotoImg from '@assets/userPhotoDefault.png'; 
 
 import { UserPhoto } from './UserPhoto';
@@ -15,7 +17,11 @@ export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto 
-        source={user.avatar  ? { uri: user.avatar } : defaulUserPhotoImg}
+        source={
+          user.avatar  
+          ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` } 
+          : defaulUserPhotoImg
+        }
         size={16}
         alt="Imagem do usuário"
         mr={4}
--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -7,10 +7,13 @@ import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import * as yup from 'yup';
 
-import { useAuth } from '@hooks/useAuth';
+
+import defaulUserPhotoImg from '@assets/userPhotoDefault.png'; 
 
 import { api } from '@services/api';
 import { AppError } from '@utils/AppError';
+
+import { useAuth } from '@hooks/useAuth';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -55,7 +58,6 @@ const profileSchema = yup.object({
 export function Profile() {
   const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
-  const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
 
   const toast = useToast();
   const { user, updateUserProfile } = useAuth();
@@ -86,7 +88,7 @@ export function Profile() {
 
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
         
-        if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 2){
+        if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
           
           return toast.show({
             title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
@@ -180,7 +182,11 @@ export function Profile() {
               />
             :
               <UserPhoto 
-                source={{ uri: userPhoto }}
+                source={
+                  user.avatar  
+                  ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` } 
+                  : defaulUserPhotoImg
+                }
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
