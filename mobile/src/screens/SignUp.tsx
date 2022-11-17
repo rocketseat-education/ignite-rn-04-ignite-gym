34359d43b89deb068f4aec81dcb999c850873011
RennanD <rplaykl@gmail.com>
2022-11-17 16:45:30 -0300

    Contextos no React Native - Criando conta e logando usuário

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,9 +1,11 @@
-import { Alert } from 'react-native';
+import { useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+import { useAuth } from '@hooks/useAuth';
 
 import { api } from "@services/api";
 
@@ -31,8 +33,10 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
 
   const toast = useToast();
+  const { singIn } = useAuth();
   
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -46,9 +50,13 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password });
-      console.log(response.data);
+      setIsLoading(true)
+
+      await api.post('/users', { name, email, password });
+      await singIn(email, password)
     } catch (error) {
+      setIsLoading(false);
+
       const isAppError = error instanceof AppError;
 
       const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
@@ -146,6 +154,7 @@ export function SignUp() {
           <Button 
             title="Criar e acessar" 
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
         
