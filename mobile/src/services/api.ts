c0040b08b47c7863764003cbbe9cba2e2061619f
RennanD <rplaykl@gmail.com>
2023-01-31 15:33:02 -0300

    Refresh Token - Reenviando a requisição

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -53,9 +53,24 @@ api.registerInterceptTokenManager = singOut => {
         return new Promise(async (resolve, reject) => {
           try {
             const { data } = await api.post('/sessions/refresh-token', { refresh_token });
-
             await storageAuthTokenSave({ token: data.token, refresh_token: data.refresh_token });
+
+            if(originalRequestConfig.data) {
+              originalRequestConfig.data = JSON.parse(originalRequestConfig.data);
+            }
+
+            originalRequestConfig.headers = { 'Authorization': `Bearer ${data.token}` };
+            api.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+
+            failedQueued.forEach(request => {
+              request.onSuccess(data.token);
+            });
+
+            console.log("TOKEN ATUALIZADO");
+
+            resolve(api(originalRequestConfig));
           } catch (error: any) {
+            console.log(error)
             failedQueued.forEach(request => {
               request.onFailure(error);
             })
